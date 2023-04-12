c8e8a8f9ede3e049bbd833a9293625a50175f932
Michal Piotrowski <michalwski@gmail.com>
2023-04-12 09:45:11 +0200

    Restore vim welcome message after initialising auto_dark plugin

--- .config/nvim/lua/michal/colors.lua
+++ .config/nvim/lua/michal/colors.lua
@@ -26,4 +26,12 @@ auto_dark_mode.setup({
 		vim.cmd('colorscheme catppuccin')
 	end,
 })
+
 auto_dark_mode.init()
+
+-- restore the intro message as the init above make is disappear
+
+local num_args = vim.api.nvim_eval(vim.api.nvim_exec('echo argc()', true))
+if num_args == 0 then
+    vim.api.nvim_exec('intro', false)
+end
