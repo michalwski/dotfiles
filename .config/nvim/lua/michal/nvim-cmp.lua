fcc8bfaf008b6c4e23146ffd837f903b947cc654
Michal Piotrowski <michalwski@gmail.com>
2023-05-21 11:23:20 +0200

    Update nvim-cmp key bindings

--- .config/nvim/lua/michal/nvim-cmp.lua
+++ .config/nvim/lua/michal/nvim-cmp.lua
@@ -12,7 +12,20 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
-		['<Tab>'] = cmp.mapping.confirm({ select = true })
+		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+		['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
+                ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
+		['<CR>'] = cmp.mapping({
+                    i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+                    c = function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                        else
+                            fallback()
+                        end
+                    end
+                })
+
 	},
 	sources = {
 		{ name = 'nvim_lsp' },
