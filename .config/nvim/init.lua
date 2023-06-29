dc54e47e36730670ea48f14b2cfa044c3a4c4d1f
Michal Piotrowski <michalwski@gmail.com>
2023-06-29 10:40:34 +0200

    Configure more formatters with null-ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -112,6 +112,21 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.prettier.with({
+            filetypes = {
+		    "javascript",
+                    "typescript",
+                    "css",
+                    "scss",
+                    "html",
+                    "json",
+                    "yaml",
+                    "markdown",
+                    "graphql",
+                    "md",
+                    "txt"
+                    }
+	}),
         null_ls.builtins.diagnostics.eslint,
 	null_ls.builtins.diagnostics.credo,
     },
